50623d0e6c49ee5b981be6e41427e409e68b6231
Claudius Joel <otiiclaudius@gmail.com>
2024-06-13 19:50:46 +0300

    Update of the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+
+const initialState = {
+  questions: [],
+
+  //'loading, 'error' , 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Action Unknown");
+  }
+};
 
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <Error />}
+      </Main>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,8 @@
+import logo from "./logo.svg";
 function Header() {
   return (
-    <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
+    <header className="app-header">
+      <img src={logo} alt="logo" />
       <h1>The React Quiz</h1>
     </header>
   );
