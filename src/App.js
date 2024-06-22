5688930f31e681a7229f3815be917acba71e336d
Claudius Joel <otiiclaudius@gmail.com>
2024-06-22 14:15:39 +0300

    Update of the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,30 +3,33 @@ import { useEffect, useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
 import Button from "../components/Button";
-import { useAuth } from "../context/FakeAuthContext";
-import { useNavigate } from "react-router-dom";
+// import { useAuth } from "../context/FakeAuthContext";
+// import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
 
-  const { login, isAuthenticated } = useAuth();
-  const navigate = useNavigate();
+  // const { login, isAuthenticated } = useAuth();
+  // const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (email && password) login(email, password);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   if (email && password) login(email, password);
+  // };
 
-  useEffect(() => {
-    if (isAuthenticated) navigate("/app", { replace: true });
-  }, [isAuthenticated, navigate]);
+  // useEffect(() => {
+  //   if (isAuthenticated) navigate("/app", { replace: true });
+  // }, [isAuthenticated, navigate]);
 
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form} onSubmit={handleSubmit}>
+      <form
+        className={styles.form}
+        // onSubmit={handleSubmit}
+      >
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
